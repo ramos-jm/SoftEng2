4edd8dcbfd664e854108d407e75f1e7305cab381
John Michael Calitis Ramos <127398189+ramos-jm@users.noreply.github.com>
2024-02-01 01:34:59 +0000

    Lab Seatwork 1

---
+++ Car.java
@@ -0,0 +1,78 @@
+public class Car{
+        protected final String brand;
+        protected final String engineType;
+        protected final String transmission;
+        protected final String color;
+        protected final Integer capacity;
+
+    public String getBrand(){
+        return brand;
+    }
+
+    public String getEngineType(){
+        return engineType;
+    }
+
+    public String transmission(){
+        return transmission;
+    }
+
+    public String color(){
+        return color;
+    }
+
+    public Integer capacity(){
+        return capacity;
+    }
+
+    private Car(CarBuilder builder){
+        this.brand = builder.brand;
+        this.engineType = builder.engineType;
+        this.transmission = builder.transmission;
+        this.color = builder.color;
+        this.capacity = builder.capacity;
+    }
+
+    protected static class CarBuilder{
+        private String brand;
+        private String engineType;
+        private String transmission;
+        private String color;
+        private Integer capacity;
+
+        public CarBuilder brand(String brand){
+            this.brand = brand;
+            return this;
+        }
+
+        public CarBuilder engineType(String engineType){
+            this.engineType = engineType;
+            return this;
+        }
+
+        public CarBuilder transmission(String transmission){
+            this.transmission = transmission;
+            return this;
+        }
+
+        public CarBuilder color(String color){
+            this.color = color;
+            return this;
+        }
+
+        public CarBuilder capacity(Integer capacity){
+            this.capacity = capacity;
+            return this;
+        }
+
+        public Car build(){
+            return new Car(this);
+        }
+    }
+
+
+    
+
+}
+
+
---
+++ CarTest.java
@@ -0,0 +1,19 @@
+public class CarTest{
+    public static void main(String[] args){
+
+        Car myCar = new Car.CarBuilder()
+        .engineType("Gasoline")
+        .transmission("Manual")
+        .color("White")
+        .capacity(8)
+        .brand("Toyota")
+        .build();
+
+        System.out.println("- Car Builder - \nInformation Details");
+        System.out.println("Brand: "+ myCar.brand);
+        System.out.println("Transmission: "+ myCar.transmission);
+        System.out.println("Engine Type"+ myCar.engineType);
+        System.out.println("Color: "+ myCar.color);
+        System.out.println("Capacity: "+ myCar.capacity);
+}
+}
